63d4d463950ef533638bc29bced5a359a2ba2baf
jaros <judin.jaroslav@gmail.com>
2019-01-05 11:43:19 +0100

    test k param calculation

--- test.js
+++ test.js
@@ -6,6 +6,20 @@ const client = require('./client')
 const server = require('./server')
 const SRPInteger = require('./lib/srp-integer')
 
+const { BigInteger } = require('jsbn')
+
+const params = require('./lib/params')
+
+describe('test params', () => {
+  it('calculate k param', () => {
+    const kHex = params.k.toHex()
+    console.log(kHex)
+    const kBi = new BigInteger(kHex, 16)
+    console.log(kBi.toString(16))
+    assert.strictEqual(kBi.toString(16), '5b9e8ef059c6b32ea59fc1d322d37f04aa30bae5aa9003b8321e21ddb04e300')
+  })
+})
+
 describe('Secure Remote Password', () => {
   it('should authenticate a user', () => {
     const username = 'linus@folkdatorn.se'
