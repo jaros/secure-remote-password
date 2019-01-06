da7c7bbf0cc14f328322d466751915990bb84180
jaros <judin.jaroslav@gmail.com>
2019-01-06 11:56:37 +0100

    omit padStart zero in HEX representation
    test x (private key) calculation - adjust with bouncycastle

--- __test__/it.test.js
+++ __test__/it.test.js
@@ -10,10 +10,10 @@ const { BigInteger } = require('jsbn')
 
 const api = apisauce.create({
   baseURL: 'http://localhost:8080',
-  timeout: 15000
+  timeout: 40000
 })
 
-jest.setTimeout(16000)
+jest.setTimeout(45000)
 
 describe('call api', () => {
   const srpClient = client1024Bit
@@ -55,6 +55,24 @@ describe('test params', () => {
     const kBi = new BigInteger(kHex, 16)
     console.log(kBi.toString(16))
     expect(kBi.toString(16)).toBe('1a1a4c140cde70ae360c1ec33a33155b1022df951732a476a862eb3ab8206a5c')
+  })
+
+  test('calculate x param at 1024-bit group', () => {
+    let params = client1024Bit.params()
+    console.log(params.N.toString())
+    console.log(params.g.toString())
+
+    let expectedX = '65ac38dff8bc34ae0f259e91fbd0f4ca2fa43081c9050cec7cac20d015f303'
+
+    let I = 'alice'
+    let P = 'password123'
+    let s = 'BEB25379D1A8581EB5A727673A2441EE'
+
+    const xHex = client1024Bit.derivePrivateKey(s, I, P)
+
+    console.log('calculated x', xHex)
+
+    expect(xHex).toBe(expectedX)
   })
 
   test('calculate k param at 2048-bit group', () => {
