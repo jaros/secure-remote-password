5d69fad7217cbf7e3777088136076f86ebf5f2fe
jaros <judin.jaroslav@gmail.com>
2019-01-06 12:27:17 +0100

    test verifier

--- __test__/it.test.js
+++ __test__/it.test.js
@@ -41,6 +41,55 @@ describe('call api', () => {
     expect(res).toBeDefined()
   })
 
+  test('login', async () => {
+    const stepZero = await api.post('/auth/session', {}, {
+      headers: {
+        Accept: '*/*'
+      },
+      params: {
+        login: username
+      }
+    })
+    const sessionId = stepZero.data
+
+    const stepOne = await api.post('/auth/challenge', {}, {
+      params: {sessionId}
+    })
+    const challenge = stepOne.data
+    console.log(challenge)
+
+    const privateKey = client1024Bit.derivePrivateKey(challenge.s, username, password) // x
+
+    const clientEphemeral = client1024Bit.generateEphemeral() // A and a
+    console.log('client Aa', clientEphemeral)
+
+    const stepTwo = await api.post('/auth/challenge/a', {}, {
+      params: {
+        A: SRPInteger.fromHex(clientEphemeral.public).toString(),
+        sessionId
+      }
+    })
+
+    const serverEphemeralPublic = SRPInteger.fromDecimal(stepTwo.data.B)
+
+    console.log('got server public B', serverEphemeralPublic.toString())
+
+    const clientSession = client1024Bit.deriveSession(clientEphemeral.secret, serverEphemeralPublic.toHex(), challenge.s, username, privateKey)
+// try to calculate M1 in a simple way like in bouncycastle lib
+    const M1 = SRPInteger.fromHex(clientSession.proof).toString()
+
+    const stepThree = await api.post('/auth/challenge/m', {}, {
+      params: {
+        M1,
+        sessionId
+      }
+    })
+
+    const serverProof = stepThree.data.M2
+    console.log('M2:', serverProof)
+    // client.verifySession(clientEphemeral.public, clientSession, serverSession.proof)
+  })
+
   test('ping server', async () => {
     const res = await api.get('/ping')
     console.log(res)
@@ -56,7 +105,7 @@ describe('test params', () => {
     expect(kHex).toBe('1a1a4c140cde70ae360c1ec33a33155b1022df951732a476a862eb3ab8206a5c')
   })
 
-  test('calculate x param at 1024-bit group', () => {
+  test('calculate x at 1024-bit group', () => {
     let params = client1024Bit.params()
     console.log(params.N.toString())
     console.log(params.g.toString())
@@ -71,6 +120,23 @@ describe('test params', () => {
 
     console.log('calculated x', xHex)
     expect(withoutLeadingZeros(xHex)).toBe(expectedX)
+  })
+
+  test('calculate verifier at 1024-bit group', () => {
+    let params = client1024Bit.params()
+    console.log(params.N.toString())
+    console.log(params.g.toString())
+
+    let expectedV = '27e2855ac715f625981dba238667955db341a3bdd919868943bc049736c7804cd8e0507dfefbf5b8573f5aae7bac19b257034254119ab520e1f7cf3f45d01b159016847201d14c8dc95ec34e8b26ee255bc4cb28d4f97e0db97b65bdd196c4d2951cd84f493afd7b34b90984357988601a3643358b81689dfd0cb0d21e21cf6e'
+
+    let I = 'alice'
+    let P = 'password123'
+    let s = 'BEB25379D1A8581EB5A727673A2441EE'
+
+    const verifier = client1024Bit.deriveVerifier(client1024Bit.derivePrivateKey(s, I, P))
+
+    console.log('calculated verifier', verifier)
+    expect(withoutLeadingZeros(verifier)).toBe(expectedV)
   })
 
   test('calculate k param at 2048-bit group', () => {
